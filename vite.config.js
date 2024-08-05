8dc9cd83c8b1659db389024a73fcf7230f0421b7
Hitesh Kumar <145363832+Hiteshpan@users.noreply.github.com>
2024-08-06 01:46:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
-  },
-  server: {
-    fs: {
-      allow: ["."],
-    },
-  },
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'  // Adjust this path if your index.html is in a different location
+    }
+  }
 });
